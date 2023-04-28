4ef59fb78f12a6a8ece77cbab347dfdda1075f1b
huangbqsky <huangbqsky@gmail.com>
2023-04-28 17:07:28 +0800

    window click

--- src/main.rs
+++ src/main.rs
@@ -3,30 +3,10 @@ fn main() {
 }
 
 slint::slint! {
-    // export component MainWindow inherits Window {
-    //     Text {
-    //         text: "hello world";
-    //         color: green;
-    //     }
-    // }
-
-    component MemoryTile inherits Rectangle {
-        width: 64px;
-        height: 64px;
-        background: #3960D5;
-    
-        Image {
-            source: @image-url("icons/bus.png");
-            width: parent.width;
-            height: parent.height;
-        }
-        Text {
-            text: "hello world";
-            color: red;
-        }
-    }
-    
     export component MainWindow inherits Window {
-        MemoryTile {}
+        Text { // Text 文本
+            text: "hello world";
+            color: green;
+        }
     }
 }

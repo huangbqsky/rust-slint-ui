8cdd159b731dbfe808a51fa1a2f76801a4a40d17
huangbqsky <huangbqsky@gmail.com>
2023-05-11 12:56:14 +0800

    slint slide_puzzle 游戏

--- build.rs
+++ build.rs
@@ -1,3 +1,6 @@
 fn main() {
-    slint_build::compile("ui/simple.slint").unwrap();
+
+    // slint_build::compile("ui/main.slint").unwrap();
+    // slint_build::compile("ui/simple.slint").unwrap();
+    slint_build::compile("ui/slide_puzzle.slint").unwrap();
 }

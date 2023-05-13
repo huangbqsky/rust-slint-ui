9b99f77a10ed2c09f9ec4c0fa18ad2ea7001d241
huangbqsky <huangbqsky@gmail.com>
2023-05-13 18:40:48 +0800

    Slint if条件判断和动画

--- examples/widget_example_for.rs
+++ examples/widget_example_for.rs
@@ -1,14 +1,13 @@
-
 fn main() {
     Example::new().unwrap().run().unwrap();
 }
 
 // slint宏，构建 UI
-slint::slint!{
-  
+slint::slint! {
+
     // 使用for-in语法多次创建一个元素： for name[index] in model : id := Element { ... }
     // 在元素中进行查找，索引是可选的，将被设置为该元素在模型中的索引。id也是可选的。
-    
+
     export component Example inherits Window {
         preferred-width: 600px;
         preferred-height: 300px;
@@ -27,7 +26,7 @@ slint::slint!{
                     text: data.foo;
                 }
             }
-         
+
             HorizontalLayout {
                  // 组类类型模型：将为数组或模型中的每个元素实例化元素
                 for my-color[index] in [ #e11, #1a2, #23d ]: Rectangle {
@@ -36,6 +35,22 @@ slint::slint!{
                     background: my-color;
                 }
             }
-        } 
+
+            Rectangle {
+                // 条件：仅当给定条件为真时，构造if才会实例化元素。语法是if condition : id := Element { ... }
+                if area.pressed : foo := Rectangle { background: blue;}
+                if !area.pressed : Rectangle { background: red;}
+                area := TouchArea {}
+            }
+
+            Rectangle {
+                background: area1.pressed ? red : blue;
+                // 动画: 使用animate这样的关键字声明属性的动画
+                animate background {
+                    duration: 250ms;
+                }
+                area1 := TouchArea {}
+            }
+        }
     }
 }

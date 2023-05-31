81fbc4e567ee54531287058aa1c51d6df5a451a3
huangbqsky <huangbqsky@gmail.com>
2023-05-31 15:27:55 +0800

    slint ui global

--- examples/widget_global.rs
+++ examples/widget_global.rs
@@ -1,5 +1,9 @@
+use std::rc::Rc;
+
+use slint::{StandardListViewItem, VecModel};
+
 slint::slint!{
-    import { GroupBox, HorizontalBox, VerticalBox, LineEdit, Button } from "std-widgets.slint";
+    import { GroupBox, HorizontalBox, VerticalBox, StandardListView, LineEdit, Button } from "std-widgets.slint";
     
     // 导出全局单单例
     export global Logic {
@@ -7,8 +11,20 @@ slint::slint!{
         in-out property <int> counter: 0;
         // 全局单例中的回调（带参数和返回值）
         pure callback to-upper-case(string) -> string;
-        // Do the translation of the first argument, with an array of string as supstitution
-        pure callback gettext(string, [string]) -> string;
+    }
+
+    // 对外导出全局单例（可与 Rust 代码直接交互）
+    export global ListViewPageAdapter  {
+        // StandardListView model (in StandardListViewItem): The model
+        in property <[StandardListViewItem]> list_data: [
+            {text: "Lorem"}, {text: "ipsum"},{text: "dolor"},{text: "sit"},{text: "amet"},{text: "consetetur"},
+            {text: "Lorem"}, {text: "ipsum"},{text: "dolor"},{text: "sit"},{text: "amet"},{text: "consetetur"},
+            {text: "Lorem"}, {text: "ipsum"},{text: "dolor"},{text: "sit"},{text: "amet"},{text: "consetetur"},
+            {text: "Lorem"}, {text: "ipsum"},{text: "dolor"},{text: "sit"},{text: "amet"},{text: "consetetur"},
+            {text: "Lorem"}, {text: "ipsum"},{text: "dolor"},{text: "sit"},{text: "amet"},{text: "consetetur"},
+            {text: "Lorem"}, {text: "ipsum"},{text: "dolor"},{text: "sit"},{text: "amet"},{text: "consetetur"},
+            {text: "Lorem"}, {text: "ipsum"},{text: "dolor"},{text: "sit"},{text: "amet"},{text: "consetetur"},
+        ];
     }
     
     export component MainWindow inherits Window {
@@ -21,7 +37,7 @@ slint::slint!{
         VerticalBox {
             alignment: start;
             GroupBox {
-                title: "Rust Modify global Logic";
+                title: "Rust Global Callbacks";
                 VerticalBox {
                     input := LineEdit {
                         text: "Text to be transformed";
@@ -39,11 +55,31 @@ slint::slint!{
             }
 
             GroupBox {
-                title: "Rust Modify Property";
+                title: "Rust Modify Propertys";
                 button := Button {
                     height:30px;
                     // 属性绑定
                     text: "Button, pressed " + root.counter + " times";
+                }
+            }
+
+            GroupBox {  
+                title: "Rust Modify Models";
+                vertical-stretch: 0;
+                height: 300px;
+                StandardListView {
+                    // model方案1: 直接写死数组
+                    // model: [
+                    //     {text: "Lorem"}, {text: "ipsum"},{text: "dolor"},{text: "sit"},{text: "amet"},{text: "consetetur"},
+                    //     {text: "Lorem"}, {text: "ipsum"},{text: "dolor"},{text: "sit"},{text: "amet"},{text: "consetetur"},
+                    //     {text: "Lorem"}, {text: "ipsum"},{text: "dolor"},{text: "sit"},{text: "amet"},{text: "consetetur"},
+                    //     {text: "Lorem"}, {text: "ipsum"},{text: "dolor"},{text: "sit"},{text: "amet"},{text: "consetetur"},
+                    //     {text: "Lorem"}, {text: "ipsum"},{text: "dolor"},{text: "sit"},{text: "amet"},{text: "consetetur"},
+                    //     {text: "Lorem"}, {text: "ipsum"},{text: "dolor"},{text: "sit"},{text: "amet"},{text: "consetetur"},
+                    //     {text: "Lorem"}, {text: "ipsum"},{text: "dolor"},{text: "sit"},{text: "amet"},{text: "consetetur"},
+                    // ];
+                    // model方案2: 可与 Rust 代码动态交互的数据
+                    model: ListViewPageAdapter.list-data;
                 }
             }
         }
@@ -68,6 +104,15 @@ fn main() {
         main_window.set_counter(value);
     });
 
+    // 获取 StandardListView Model数据，这里用到 VecModel，才能修改值
+    let tiles_model: Rc<VecModel<StandardListViewItem>> = Rc::new(VecModel::default());
+    // 重新组织填充数据： 100列的数据
+    for r in 1..=100 { // 100行
+        tiles_model.push(slint::format!("Item {r}").into());
+    }
+    // 通过全局单例重新设置list_data的值， set_XXXX()是重新设置Model数据
+    window.global::<ListViewPageAdapter>().set_list_data(tiles_model.clone().into());
+
+    // 运行窗体
     window.run().unwrap();
-    // ...
 }
